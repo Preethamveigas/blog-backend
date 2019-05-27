663427fd2f9b4fbe2147463d605ffc385340f490
Preethamveigas <50978555+Preethamveigas@users.noreply.github.com>
2019-05-27 16:42:51 +0530

    Updated key for the mongo
    Uri is changed

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://preetham:preetham@cluster0-zcefm.mongodb.net/test?retryWrites=true',
+//   mongoURI: 'mongodb+srv://preetham:preetham@cluster0-zcefm.mongodb.net/test?retryWrites=true',
+  mongoURI: 'mongodb+srv://honey:<password>@cluster0-tl3sm.gcp.mongodb.net/test?retryWrites=true',
 }
